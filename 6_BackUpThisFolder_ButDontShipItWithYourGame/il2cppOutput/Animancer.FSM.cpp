fad6fbc0653f1efe4ff0d04c0f1d9d3a9069b9cd
ahmtkrbgli <ahmtkrbgli@gmail.com>
2023-01-16 21:54:34 +0300

    v 0.39

---
+++ 6_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Animancer.FSM.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct U3CModuleU3E_t61C641BC2B97671AB4A8C042FCB86281B472435E 
+{
+};
+
+// Animancer.FSM.StateExtensions
+struct StateExtensions_t3CF179200F5EDB7A449C1A65D2BBDD9F3E0DC28B  : public RuntimeObject
+{
+};
+
+// <Module>
+
+// <Module>
+
+// Animancer.FSM.StateExtensions
+
+// Animancer.FSM.StateExtensions
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
